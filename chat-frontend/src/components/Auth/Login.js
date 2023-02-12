efbcf401a59e1a7f5672af47e140f301281ba6b7
Sumit Gautam <sghost33@gmail.com>
2023-02-12 22:37:20 +1100

    Add node-sass to compile scss and setting up login screen styling and structure

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import loginImage from '../../assets/images/login.svg'
 
 const Login = () => {
-  return <h1>Login screen</h1>
+  return (
+    <div id="auth-container">
+      <div id="auth-card">
+        <div>
+          <div id="image-section">
+            <img src={loginImage} alt="Login" />
+          </div>
+          <div id="form-section">
+            <h2>Welcome Back</h2>
+            <form>
+              <div className="input-field">
+                <input placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <input placeholder="Password" />
+              </div>
+              <button>LOGIN</button>
+            </form>
+            <p>Don't have an account? Register</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
