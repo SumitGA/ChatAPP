8ec2e3e5991f0ba10f979cee39d18acf062824c6
Sumit Gautam <sghost33@gmail.com>
2023-02-12 22:57:02 +1100

    Login page styling completed

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -6,17 +6,17 @@ const Login = () => {
   return (
     <div id="auth-container">
       <div id="auth-card">
-        <div>
+        <div className="card-shadow">
           <div id="image-section">
             <img src={loginImage} alt="Login" />
           </div>
           <div id="form-section">
             <h2>Welcome Back</h2>
             <form>
-              <div className="input-field">
+              <div className="input-field mb-1">
                 <input placeholder="Email" />
               </div>
-              <div className="input-field">
+              <div className="input-field mb-2">
                 <input placeholder="Password" />
               </div>
               <button>LOGIN</button>
