209462a8e4df93a6af96f5d028f3443de2de4d5b
Sumit Gautam <sghost33@gmail.com>
2023-02-12 14:32:01 +1100

    Adding user login logic to auth controller and using bcrypt to check if password matches

--- chat-backend/controllers/authController.js
+++ chat-backend/controllers/authController.js
@@ -1,9 +1,31 @@
-exports.login = async(req, res) => {
-  const {email, password} = req.body
-  return res.send([email, password])
+const User = require('../models/').User
+const bcrypt = require('bcrypt')
 
+exports.login = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    // find the user
+    const user = await User.findOne({
+      where: {
+        email
+      }
+    })
+    // check if user found
+    if(!user) return res.status(404).json({message: 'User not found!'})
+
+    // check if password matches
+    if(!bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({message: 'Incorrect password!'})
+    }
+    
+    // generate auth token
+
+    return res.send(user)
+
+  } catch (e) {
+
+  }
 }
 
-exports.register = async(req, res) => {
-
-}
+exports.register = async (req, res) => {}
