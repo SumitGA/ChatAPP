e0939f4d4e784fdf74f09964208fe228215d2c6f
Sumit Gautam <sghost33@gmail.com>
2023-02-12 14:47:44 +1100

    Finishing up login function and updating user token with secrete keys stored in private file

--- chat-backend/controllers/authController.js
+++ chat-backend/controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require('../models/').User
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('../config/app')
 
 exports.login = async (req, res) => {
   const { email, password } = req.body
@@ -8,24 +10,32 @@ exports.login = async (req, res) => {
     // find the user
     const user = await User.findOne({
       where: {
-        email
-      }
+        email,
+      },
     })
     // check if user found
-    if(!user) return res.status(404).json({message: 'User not found!'})
+    if (!user) return res.status(404).json({ message: 'User not found!' })
 
     // check if password matches
-    if(!bcrypt.compareSync(password, user.password)) {
-      return res.status(401).json({message: 'Incorrect password!'})
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ message: 'Incorrect password!' })
     }
-    
+
     // generate auth token
-
-    return res.send(user)
-
+    const userWithToken = generateToken(user.get({ raw: true }))
+    return res.send(userWithToken)
   } catch (e) {
-
+    return res.status(500).json({ message: e.message })
   }
 }
 
 exports.register = async (req, res) => {}
+
+const generateToken = (user) => {
+  console.log(user)
+  delete user.password
+
+  const token = jwt.sign(user, config.appKey, { expiresIn: 86400 })
+
+  return { ...user, ...{ token } }
+}
