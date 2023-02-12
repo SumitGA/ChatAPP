9587fc628c826363546edf4bdca5a8ab6056e410
Sumit Gautam <sghost33@gmail.com>
2023-02-12 15:28:32 +1100

    Extract validation logic and rules to seperate file inside of validatiors and remove them from controllers

--- chat-backend/router/auth.js
+++ chat-backend/router/auth.js
@@ -1,20 +1,13 @@
 const router = require('express').Router()
 const { login, register } = require('../controllers/authController')
 const { body } = require('express-validator')
+const { validate } = require('../validators')
+const { rules: registrationRules } = require('../validators/auth/register')
+const { rules: loginRules } = require('../validators/auth/login')
 
-router.post('/login', login)
+router.post('/login', [loginRules, validate], login)
 
 // Using express validator to validate user inputs although we have implemented DB constraints.
-router.post(
-  '/register',
-  [
-    body('firstName').notEmpty(),
-    body('lastName').notEmpty(),
-    body('email').isEmail(),
-    body('password').isLength({ min: 5 }),
-    body('gender').notEmpty(),
-  ],
-  register,
-)
+router.post('/register', [registrationRules, validate], register)
 
 module.exports = router
