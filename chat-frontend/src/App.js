25e7139a35a8666415601551507c952fa7a682b3
Sumit Gautam <sghost33@gmail.com>
2023-02-12 22:21:36 +1100

    Add react-router-dom and used router, routes along with elements to be shown based on url routes

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -3,13 +3,20 @@ import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 import Chat from './components/Chat/Chat'
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      <Chat />
-      <Login />
-      <Register />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Chat />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route render={() => <h1>404 page not found</h1>} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
