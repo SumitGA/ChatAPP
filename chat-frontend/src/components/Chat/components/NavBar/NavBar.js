4d91fd313e182d39566bab14a5662e9eaf85220d
Sumit Gautam <sghost33@gmail.com>
2023-02-17 14:57:46 +1100

    Use state to toggle carretdown icon to show profileoptions

--- chat-frontend/src/components/Chat/components/NavBar/NavBar.js
+++ chat-frontend/src/components/Chat/components/NavBar/NavBar.js
@@ -1,25 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './NavBar.scss'
 
 const NavBar = () => {
   const user = useSelector((state) => state.authReducer.user)
+  const [showProfileOptions, setShowProfileOptions] = useState(false)
 
   return (
     <div id="navbar" className="card-shadow">
       <h2>Chat.io</h2>
-      <div id="profile-menu">
+      <div
+        onClick={() => setShowProfileOptions(!showProfileOptions)}
+        id="profile-menu"
+      >
         <img width="40" height="40" src={user.avatar} alt="Avatar"></img>
         <p>
           {user.firstName} {user.lastName}
         </p>
         <FontAwesomeIcon icon="caret-down" className="fa-icon" />
-
-        <div id="profile-options">
-          <p>Update Profile</p>
-          <p>Logout</p>
-        </div>
+        {showProfileOptions && (
+          <div id="profile-options">
+            <p>Update Profile</p>
+            <p>Logout</p>
+          </div>
+        )}
       </div>
     </div>
   )
