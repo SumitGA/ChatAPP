bee536afa4954646c6fa71038301d8981684e647
Sumit Gautam <sghost33@gmail.com>
2023-02-17 12:09:00 +1100

    Creating login reducer using redux

---
+++ chat-frontend/src/store/reducers/auth.js
@@ -0,0 +1,25 @@
+import { LOGIN } from '../actions/auth'
+
+const initialState = {
+  user: {},
+  token: '',
+  isLoggedIn: false,
+}
+
+const authReducer = (state = initialState, action) => {
+  const { type, payload } = action
+
+  switch (type) {
+    case LOGIN:
+      return {
+        ...state,
+        user: payload,
+        token: payload.token,
+        isLoggedIn: true,
+      }
+    default:
+      return state
+  }
+}
+
+export default authReducer
