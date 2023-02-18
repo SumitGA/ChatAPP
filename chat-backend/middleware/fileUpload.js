86ca7bf0ebcc4764fd3398a9b7e4e78502df62a1
Sumit Gautam <sghost33@gmail.com>
2023-02-18 13:03:08 +1100

    Creating file upload middleware with multer

---
+++ chat-backend/middleware/fileUpload.js
@@ -0,0 +1,60 @@
+const multer = require('multer')
+const fs = require('fs')
+const path = require('path')
+
+exports.userFile = (req, res, next) => {
+  const getFileType = (file) => {
+    const mimeType = file.mimeType.split('/')
+    return mimeType[mimeType.length - 1]
+  }
+
+  const generateFileName = (req, file, cb) => {
+    const extension = getFileType(file)
+    const filename =
+      Date.now() + '-' + Math.round(Math.random() * 1e9) + `.` + extension
+    cb(null, file.fieldname + '-' + filename)
+  }
+
+  const fileFilter = (req, file, cb) => {
+    const extension = getFileType(file)
+
+    const allowedTypes = /jpeg|jpg|png/
+
+    const passed = allowedTypes.test(extension)
+    if (passed) {
+      return cb(null, true)
+    }
+
+    return cb(null, false)
+  }
+
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      const { id } = req.body
+      const dest = `uploads/user/${id}`
+
+      fs.access(dest, (err) => {
+        // File does not exits if error
+        if (err) {
+          return fs.mkdir(dest, (err) => {
+            cb(err, dest)
+          })
+        } else {
+          // File does exists
+          fs.readdir(dest, (err, files) => {
+            if (err) throw err
+            for (const file of files) {
+              fs.unlink(path.join(dest, file), (err) => {
+                if (err) throw err
+              })
+            }
+          })
+          return cb(null, dest)
+        }
+      })
+    },
+    filename: generateFileName,
+  })
+  // Using multer package for file upload
+  return multer({ storage, fileFilter }).single('avatar')
+}
