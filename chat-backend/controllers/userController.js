8d25e06e1dd934e893939a8573c57c2f9f02e378
Sumit Gautam <sghost33@gmail.com>
2023-02-18 12:47:12 +1100

    Creating user update logic along with validators for validation user's field

--- chat-backend/controllers/userController.js
+++ chat-backend/controllers/userController.js
@@ -1,6 +1,20 @@
 const User = require('../models').User
 const sequelize = require('sequelize')
 
-exports.update = (req, res) => {
-  return res.send('User controller')
+exports.update = async (req, res) => {
+  try {
+    const [rows, result] = await User.update(req.body, {
+      where: {
+        id: req.user.id,
+      },
+      returning: true,
+      individualHooks: true,
+    })
+    const user = result[0].get({ raw: true })
+    user.avatar = result[0].avatar
+    delete user.password
+    return res.send(user)
+  } catch (err) {
+    return res.status(500).json({ error: err.message })
+  }
 }
--- chat-backend/router/user.js
+++ chat-backend/router/user.js
@@ -2,7 +2,8 @@ const router = require('express').Router()
 const { update } = require('../controllers/userController')
 const { validate } = require('../validators')
 const { auth } = require('../middleware/auth')
+const { rules: updateRules } = require('../validators/user/update')
 
-router.post('/update', [auth], update)
+router.post('/update', [auth, updateRules, validate], update)
 
 module.exports = router
